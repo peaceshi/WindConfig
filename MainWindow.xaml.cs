22be2f445be45e7971ccb57a9799fd4e5b368a98
peaceshi <peaceshi@outlook.com>
2022-08-16 04:09:46 +0800

    initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+
+using Wind3Config.Model;
+
+namespace Wind3Config;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Resolution_RadioButton1_Checked(object sender, RoutedEventArgs e)
+    {
+        Wind3Registry.SetResolutionValue(800, 600);
+    }
+
+    private void Resolution_RadioButton2_Checked(object sender, RoutedEventArgs e)
+    {
+        Wind3Registry.SetResolutionValue(1024, 768);
+    }
+
+    private void Window_WindowMode_Checked(object sender, RoutedEventArgs e)
+    {
+        Wind3Registry.SetIsFullscreenValue(0);
+    }
+
+    private void Window_FullScreenMode_Checked(object sender, RoutedEventArgs e)
+    {
+        Wind3Registry.SetIsFullscreenValue(1);
+    }
+}
---
+++ ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,84 @@
+﻿using System.ComponentModel;
+
+using Wind3Config.Model;
+
+namespace Wind3Config.ViewModel;
+
+public class MainWindowViewModel : INotifyPropertyChanged
+{
+    private static bool _IsFullScreenMode = true;
+    private static bool _IsWindowMode;
+    private static bool _IsResolution_1 = true;
+    private static bool _IsResolution_2;
+
+    public MainWindowViewModel()
+    {
+        if (Wind3Registry.GetIsFullscreenValue() != 0)
+        {
+            _IsFullScreenMode = true;
+            _IsWindowMode = false;
+        }
+        else
+        {
+            _IsFullScreenMode = false;
+            _IsWindowMode = true;
+        }
+        if (Wind3Registry.GetResolutionValue() == 600)
+        {
+            _IsResolution_1 = true;
+            _IsResolution_2 = false;
+        }
+        else
+        {
+            _IsResolution_1 = false;
+            _IsResolution_2 = true;
+        }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    public bool IsFullScreenMode
+    {
+        get { return _IsFullScreenMode; }
+        set
+        {
+            _IsFullScreenMode = value;
+            OnPropertyChanged(nameof(IsFullScreenMode));
+        }
+    }
+
+    public bool IsWindowMode
+    {
+        get { return _IsWindowMode; }
+        set
+        {
+            _IsWindowMode = value;
+            OnPropertyChanged(nameof(IsWindowMode));
+        }
+    }
+
+    public bool IsResolution_1
+    {
+        get { return _IsResolution_1; }
+        set
+        {
+            _IsResolution_1 = value;
+            OnPropertyChanged(nameof(IsResolution_1));
+        }
+    }
+
+    public bool IsResolution_2
+    {
+        get { return _IsResolution_2; }
+        set
+        {
+            _IsResolution_2 = value;
+            OnPropertyChanged(nameof(IsResolution_2));
+        }
+    }
+
+    protected void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
