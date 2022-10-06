874dde31685e6c9e399f22859c20fa4c2e0e095e
peaceshi <peaceshi@outlook.com>
2022-10-06 21:24:08 +0800

    add window name change

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,10 +1,38 @@
-﻿using System.Windows;
+﻿using System.IO;
+using System.Windows;
 
-namespace Wind3Config;
+using WindConfig.Model;
+
+namespace WindConfig;
 
 /// <summary>
 /// Interaction logic for App.xaml
 /// </summary>
 public partial class App : Application
 {
+    public App()
+    {
+        string Title = string.Empty;
+        string ProcessName = string.Empty;
+        if (File.Exists(Wind.Wind3ProcessName))
+        {
+            Title = Wind.Wind3ConfigTitleName;
+            ProcessName = Wind.Wind3ProcessName;
+            WindRegistry.WindKey = Wind.Wind3RegistryKeyName;
+            WindRegistry.Version = 211;
+        }
+        else if (File.Exists(Wind.Wind4ProcessName))
+        {
+            Title = Wind.Wind4ConfigTitleName;
+            ProcessName = Wind.Wind4ProcessName;
+            WindRegistry.WindKey = Wind.Wind4RegistryKeyName;
+            WindRegistry.Version = 110;
+        }
+        //else
+        //{
+        //    Current.Shutdown();
+        //}
+        MainWindow = new MainWindow(Title, ProcessName);
+        MainWindow.Show();
+    }
 }
