f956f30fe7ed53911886239f9b84b1e89854baf3
peaceshi <peaceshi@outlook.com>
2022-10-13 22:51:53 +0800

    add faq view

---
+++ View/FAQ.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+
+namespace WindConfig.View;
+
+/// <summary>
+/// FAQ.xaml 的交互逻辑
+/// </summary>
+public partial class FAQ : Window
+{
+    private readonly Stream source = Application.GetResourceStream(new Uri("pack://application:,,,/Resources/FAQ.html")).Stream;
+
+    public FAQ()
+    {
+        InitializeComponent();
+        WebBrowser.NavigateToStream(source);
+    }
+
+    private void OkButton_Click(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+}
